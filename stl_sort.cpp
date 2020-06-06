1b4764709a107c9f968865959ab8812fc62a9795
deepti451 <deepti451@github.com>
2020-06-06 20:47:06 +0530

    sort the string question

--- stl_sort.cpp
+++ stl_sort.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include<algorithm>
 #include<string>
+#include<csting>
+#include<algorithm>
 using namespace std;
-bool compare(string a,string b){
+/*bool compare(string a,string b){
     //check if both have same length check lexographically
     if (a.length()==b.length()){
         return a<b;
@@ -92,5 +94,85 @@ int main() {
    return 0;
     
 }
+*/
+// sort the strings
+string extractstringAtKey(string str, int key){
+    //strtok
+    char *s= strtok((char *)str.c_str()," ");
+    while(key>1){
+        s=strtok(NULL," ");
+        key--;
+    }
+    return (string)s; //convert string to sobject
 
+
+}
+int convertToInt(string s){
+    int ans=0;
+    int p=1;
+    for(int i=s.length()-1 ; i>=0; i--){
+        ans+= ((s[i]-'0') *p);
+        p=p*10;
+    }
+    return ans;
+
+}
+bool numericCompare(pair<string,string> s1, pair<string,string> s2){
+    string key1,key2;
+    key1=s1.second;
+    key2=s2.second;
+    return convertToInt(key1)< convertToInt(key2); 
+
+
+}
+bool lexioCompare(pair<string,string> s1, pair<string,string> s2){
+    string key1,key2;
+    key1=s1.second;
+    key2=s2.second;
+    return key1 <  key2; 
+
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cin.get();//to consume the \n after the given no
+    string a[100];
+    for (int i=0;i<n;i++){
+        getline(cin,a[i]);
+    }
+    int key;
+    string reversal, ordering;
+    cin>>key>>reversal>>ordering;
+
+    pair <string,string> strpair[100];
+    //strpair contains the string and the array
+    for (int i=0 ;i<n;i++){
+        strpair[i].first =a[i];
+        strpair[i].second =extractstringAtKey(a[i],key);
+
+    }
+    //we will do sorting//numeric-lexographically
+    if(ordering=="numeric"){
+        sort(strpair,strpair+n,numericCompare);
+    }
+    else{
+        sort(strpair,strpair+n,lexioCompare);
+    }
+    //reversal
+    if(reversal=="true"){
+        for(int i=0;i<n/2;i++){
+            swap(strpair[i],strpair[n-i-1]);
+
+        }
+    }
+    //print the output
+
+    for (int i=0;i<n ;i++){
+        cout<<strpair[i].first<<" "<<endl;
+    }
+    return 0;
+
+}
 
